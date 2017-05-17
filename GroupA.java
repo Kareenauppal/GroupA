00aee46d847634783a5d78e8d7685fd0b85c2391
Kareenauppal <kareena.uppal@gmail.com>
2017-05-17 13:51:02 -0400

    Update GroupA.java
    Added the main program and added the method for "Count Letters"

--- GroupA.java
+++ GroupA.java
@@ -3,6 +3,20 @@ class GroupA
 {
    public static void main (String[] args)
   {
-      // your program here
-   }
+     
+    
+     System.out.println("Please enter the first sentence of your favourite song");
+     
+     String sentence = In.getString();
+    
+    countLetters(sentence); // Function 1: Counting the letters
+     
+     } 
+   
+ // Method: Count Letters - Kareena Uppal
+ public static String countLetters(String sentence)
+ String sentenceLen = sentence.replaceAll("\\W+","");
+     System.out.println(sentenceLen.length());
+ 
+ 
 } 
