57b42af08ac7e830521db204a469c6018ae6ab9f
Kareenauppal <kareena.uppal@gmail.com>
2017-05-18 14:40:30 -0400

    Update GroupA.java
    made changes to the program

--- GroupA.java
+++ GroupA.java
@@ -1,4 +1,3 @@
- 
 class GroupA
 {
    public static void main (String[] args)
@@ -10,29 +9,28 @@ class GroupA
      String sentence = In.getString();
     
     countLetters(sentence); // Function 1: Counting the letters
-     
+    countWords (sentence);
      } 
    
  // Method: Count Letters - Kareena Uppal
- public static String countLetters(String sentence)
+ public static void countLetters(String sentence)
  {
  String sentenceLen = sentence.replaceAll("\\W+","");
      System.out.println(sentenceLen.length());
      
-     return sentenceLen;
+   
  }
  
  // Method: Count Words - Kenny
- public static String countWords(String sentence)
+ public static void countWords(String sentence)
  {
-    String  sentence;
-    
-    System.out.println("Please enter the first sentence of your favourite song ");
-    sentence = In.getString();
+
     
     String [] words = sentence.split("\\s+");
     
     System.out.print("Word Count : ");
     System.out.println(words.length);
+    
+
   } 
-} 
+}
