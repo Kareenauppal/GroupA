e1bf91c09e64e912aa8855baf0667fadab9a4167
kung-fu-kenny <mswap1@ocdsb.ca>
2017-05-18 15:07:23 -0400

    Update GroupA.java
    Added a method

--- GroupA.java
+++ GroupA.java
@@ -2,7 +2,7 @@ class GroupA
 {
    public static void main (String[] args)
   {
-     
+    
     
      System.out.println("Please enter the first sentence of your favourite song");
      
@@ -10,27 +10,32 @@ class GroupA
     
     countLetters(sentence); // Function 1: Counting the letters
     countWords (sentence);
+    reverseStatement (sentence);
      } 
    
  // Method: Count Letters - Kareena Uppal
- public static void countLetters(String sentence)
+ public static String countLetters(String sentence)
  {
  String sentenceLen = sentence.replaceAll("\\W+","");
      System.out.println(sentenceLen.length());
      
-   
+     return sentenceLen;
  }
  
  // Method: Count Words - Kenny
  public static void countWords(String sentence)
- {
-
-    
+ {  
     String [] words = sentence.split("\\s+");
-    
     System.out.print("Word Count : ");
     System.out.println(words.length);
-    
-
-  } 
+  }
+ 
+   // Method: Reverse Statement- Kenny
+  public static void reverseStatement(String sentence)
+  {     
+        String reverso = sentence;
+        StringBuffer reverseBuff = new StringBuffer(reverso);
+        String stringRev = reverseBuff.reverse().toString();
+        System.out.println(stringRev);      
+  }
 }
