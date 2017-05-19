e5601eb1031979087bd06ddfed4e541a31a03d6d
kung-fu-kenny <mswap1@ocdsb.ca>
2017-05-19 14:00:40 -0400

    Update GroupA.java
    added a method

--- GroupA.java
+++ GroupA.java
@@ -1,30 +1,21 @@
-class GroupA
-{
-   public static void main (String[] args)
-  {
-    
-    
-     System.out.println("Please enter the first sentence of your favourite song");
-     
      String sentence = In.getString();
-    
     
     countLetters(sentence); // Function 1: Counting the letters
     countWords (sentence);
-    reverseStatement(sentence);
-    alternateCase(sentence);
-    
-    
+    reverseStatement (sentence);
+    reverseWords (sentence);
+    alternateCase (sentence);
+      
      } 
    
  // Method: Count Letters - Kareena Uppal
- public static void countLetters(String sentence)
+ public static String countLetters(String sentence)
  {
- String sentenceLen = sentence.replaceAll("\\W+","");
-     System.out.print("Letters Count : ");
+     String sentenceLen = sentence.replaceAll("\\W+","");
+     System.out.print("Letter Count  : ");
      System.out.println(sentenceLen.length());
      
-   //  return sentenceLen;
+     return sentenceLen;
  }
  
  // Method: Count Words - Kenny
@@ -35,27 +26,55 @@ class GroupA
     System.out.println(words.length);
   }
  
-   // Method: Reverse Statement- Kenny
+ // Method:  Reverse Statement - Kenny
   public static void reverseStatement(String sentence)
   {     
         String reverso = sentence;
         StringBuffer reverseBuff = new StringBuffer(reverso);
         String stringRev = reverseBuff.reverse().toString();
-        System.out.println(stringRev);  
-        
+        System.out.print("Reverse Statement  : ");
+        System.out.println(stringRev); 
+       
   }
-  
-  // Method: Alternate Case - Kareena Uppal
+       
+
+// Method: Reverse Words - Kenny
+   public static void reverseWords(String sentence)
+  {
+     System.out.println(sentence);
+     String[] words = sentence.split("\\s+");
+     
+     String reverseSentence = "";
+     System.out.print("Reverse Words  : ");
+    
+     for (String word : words)
+  {
+    String reverseWord = "";
+    
+    for (int i = word.length()-1; i >= 0; i--)
+    reverseWord += word.charAt(i);
+    reverseSentence += reverseWord;
+    reverseSentence += " ";
+    
+    System.out.print(reverseWord + " ");
+  }
+    reverseSentence.trim();
+    
+  }
+   
+   
+   // Method: Alternate Case - Kareena Uppal
   public static void alternateCase(String sentence)
-{
-  
-    System.out.println("Please enter the first sentence of your favourite song");
-    String str = In.getString();
+  {
+    //System.out.print("Please enter the first sentence of your favourite song");
+    String str = sentence;
    
     char [] chr= str.toCharArray();
     int n = chr.length;
     char ch;
     int i;
+    System.out.println(" "); 
+    System.out.print("Alternate Case : "); 
     for(i = 0; i < n; i++) {
       
       if(i % 2 == 0) {
@@ -72,5 +91,6 @@ class GroupA
      }
   }
 
-  }
+}
+  
     
